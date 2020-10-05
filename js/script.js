e04c3fa81a49d8e52288eb93c6017a6ad6bf4f76
NoisyMrSpoon <72073483+NoisyMrSpoon@users.noreply.github.com>
2020-10-05 02:10:49 +0100

    Update script.js
    Might have sorted it, still complete with debug messages

--- js/script.js
+++ js/script.js
@@ -127,7 +127,8 @@ function buildAndShowHomeHTML (categories) {
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("#main-content", homeHtml);
+      console.log(homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content", homeHtmltoInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
